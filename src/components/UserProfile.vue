61194cd60ab1471e2995ab90f1d887bbf1a1d275
kribo <kribo@outlook.com>
2021-03-08 20:20:00 +0100

    Refactoring behind the scenes

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__follower-count">
+        <strong>Followers: </strong>{{ followers }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"UserProfile",
+    data(){
+    return {
+     followers: 0,
+     user: {
+       id: 1,
+       username: '_MitchellRomney',
+       firstName: 'Mitchell',
+       lastName: 'Romney',
+       email: 'mitchellromney@tehearthissquare.com',
+       isAdmin: true
+     }
+    }
+  },
+  /**
+   *  Watch generally used for server changes ex. notifications
+   */
+  watch:{
+    followers(newFollowerCount, oldFollowerCount){
+      if( oldFollowerCount > newFollowerCount){
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed:{
+    fullname(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+   methods: {
+    followUser(){
+      this.followers++;
+    }
+  },
+  mounted(){
+    this.followUser();
+  }
+
+}
+</script>
+
+<style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+}
+
+h1 {
+  margin: 0;
+}
+</style>
